7469c40c1502290d4b7025d058ddc2cbae5c845b
kimtaeyeon10 <taeny_kk@naver.com>
2022-06-09 08:01:12 +0900

    mapper error 수정 및 CRUD 구현

--- MemberManagement/src/main/java/com/spring_management/mapper/MemMapper.java
+++ MemberManagement/src/main/java/com/spring_management/mapper/MemMapper.java
@@ -9,4 +9,8 @@ import com.spring_management.domain.MemVo;
 public interface MemMapper {
 
 	public List<MemVo> getList();
+	public void enrollMem(MemVo mem);
+	public MemVo getMem(Long bno);
+	public int delMem(Long bno);
+	public int updateMem(MemVo mem);
 }
--- MemberManagement/src/test/java/com/spring_management/mapper/MemMapperTests.java
+++ MemberManagement/src/test/java/com/spring_management/mapper/MemMapperTests.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import com.spring_management.domain.MemVo;
+
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
@@ -21,4 +23,39 @@ public class MemMapperTests {
 	public void testGetList() {
 		mapper.getList().forEach(mem -> log.info(mem));
 	}
+	
+	@Test
+	public void testenrollMem() {
+		MemVo mem = new MemVo();
+		mem.setName("홍길");
+		mem.setTel("010-1111-5555");
+	
+		mapper.enrollMem(mem);
+		
+		log.info(mem);
+	}
+	
+	@Test
+	public void testGetMem() {
+		MemVo mem = mapper.getMem(21L);
+		
+		log.info(mem);
+	}
+	
+	@Test
+	public void testDelMem() {
+		log.info("DELETE : " + mapper.delMem(22L));
+	}
+	
+	@Test
+	public void testUpdateMem() {
+		MemVo mem = new MemVo();
+		mem.setBno(23L);
+		mem.setName("김수정");
+		mem.setTel("000-0000-0000");
+		
+		int count = mapper.updateMem(mem);
+		log.info("UPDATE : " +count);
+	}
+	
 }
