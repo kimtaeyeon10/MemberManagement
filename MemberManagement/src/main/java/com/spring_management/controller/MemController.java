49f3b5c75953c20712023f958f25ad9098ed0213
kimtaeyeon10 <taeny_kk@naver.com>
2022-06-16 12:50:43 +0900

    complete controller

--- MemberManagement/src/main/java/com/spring_management/controller/MemController.java
+++ MemberManagement/src/main/java/com/spring_management/controller/MemController.java
@@ -1,8 +1,14 @@
 package com.spring_management.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.spring_management.domain.MemVo;
 import com.spring_management.service.MemService;
 
 import lombok.RequiredArgsConstructor;
@@ -10,14 +16,45 @@ import lombok.extern.log4j.Log4j;
 
 @Controller
 @Log4j
+@RequestMapping("/member/*")
 @RequiredArgsConstructor //final
 public class MemController {
-
-	@GetMapping("/list")
-	public void list() {
-		log.info("list....=======");
-	}
+	
 	private final MemService service;
 	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("===============list....==========");
+
+		model.addAttribute("memList",service.getAllMem());
+	}
+
+	@PostMapping("/register")
+	public String register(MemVo mem,RedirectAttributes rttr) {
+		/*
+		 * log.info("mem" + mem);
+		 * 
+		 * Long bno = service.enroll(mem);
+		 * 
+		 * log.info("bno:" + bno); rttr.addFlashAttribute("result",bno);
+		 */
+		
+		return "redirect:/member/list";
+	}
 	
+	@PostMapping("/modify")
+	public String moidfy(MemVo mem, RedirectAttributes rttr) {
+		if(service.update(mem)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/member/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		if(service.delete(bno)) {
+			rttr.addFlashAttribute("result","success");
+		}
+		return "redirect:/member/list";
+	}
 }
--- MemberManagement/src/test/java/com/spring_management/controller/MemControllerTests.java
+++ MemberManagement/src/test/java/com/spring_management/controller/MemControllerTests.java
@@ -1,9 +1,15 @@
 package com.spring_management.controller;
 
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.spring_management.mapper.MemMapperTests;
@@ -12,6 +18,7 @@ import lombok.Setter;
 import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
 @ContextConfiguration({
 		"file:src/main/webapp/WEB-INF/spring/root-context.xml"
 		,"file:src/main/webapp/WEB-INF/spring/appServlet/servlet-context.xml"}
@@ -20,9 +27,57 @@ import lombok.extern.log4j.Log4j;
 public class MemControllerTests {
 
 	@Setter(onMethod_ = {@Autowired})
-	private webApplicationContext ctx;
+	private WebApplicationContext ctx;
 	
-	private MocMvc mockmvc;
+	private MockMvc mockMvc;
 	
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx).build();
+	}
+
+	@Test
+	public void testList() throws Exception {
+		log.info(mockMvc.perform(
+				MockMvcRequestBuilders.get("/member/list"))
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+	}
 	
+	@Test
+	public void testRegister() throws Exception {
+		log.info(mockMvc.perform(
+				MockMvcRequestBuilders.post("/member/register")
+				.param("name", "테이름")
+				.param("tel", "010-7777-7777")
+				)
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+	}
+	
+	@Test
+	public void testModify() throws Exception {
+		log.info(mockMvc.perform(
+				MockMvcRequestBuilders.post("/member/modify")
+				.param("bno", "24")
+				.param("name", "수정함")
+				.param("tel", "777-7777-7777")
+				)
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+	}
+	
+	@Test
+	public void testRemove() throws Exception {
+		log.info(mockMvc.perform(
+				MockMvcRequestBuilders.post("/member/remove")
+				.param("bno", "26")
+				)
+				.andReturn()
+				.getModelAndView()
+				.getModelMap());
+	}
 }
